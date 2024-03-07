fb7e07eb9ee1169c09899bab764b71ad1647911c
Amal kr <amalkr2001@gmail.com>
2024-03-07 16:07:59 +0530

    sixth commit

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = 'http://localhost:7070';
-const API_BASE_URL = 'https://machine-test-backend-26-02-2024.vercel.app/';
+const API_BASE_URL = 'https://machine-test-backend-26-02-2024.vercel.app';
 
 
 export default API_BASE_URL;
